e014d202ce16694287a3420abb769717a4f0d527
claudiu <claudiu.garba@gmail.com>
2013-11-28 23:01:11 +0100

    some tests
    for me

--- project.rb
+++ project.rb
@@ -1,5 +1,22 @@
 require 'nokogiri'
+require 'open-uri'
 class Project
+  data = []
+  site = 'http://www.park-one.com/standorte/'
+  page = Nokogiri::HTML(open(site))
+  puts page.title
+  page.css('#cc-matrix-1529124884').each do |node|
+     node.css('#cc-m-6520934484').each do |s|
+       s.css('a').each do |link|
+         data.push(link['href'])
+       end
+     end
+  end
+  data.delete_if{|m| m.include?('mail')}
+  puts data
 
-
+  page1 = Nokogiri::HTML(open('http://www.park-one.com/standorte/süd-münchen/'))
+  page1.each do |node|
+    puts node.css('#cc-m-6520329884').text
+  end
 end
