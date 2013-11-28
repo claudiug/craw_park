383bb989517480b8faf93074586d3b32863be728
claudiu <claudiu.garba@gmail.com>
2013-11-28 22:08:05 +0100

    add some toto

--- db_connect.rb
+++ db_connect.rb
@@ -1,2 +1,24 @@
 class DbConnect
+
+  module ENV
+    URL = ""
+    PASS = ""
+    DBNAME = ""
+  end
+
+
+  private
+
+  def get_connection_status
+    #TODO check the status of connection, use rescue when is wrong
+  end
+
+  def reconnect
+    #TODO reconnect when is down
+  end
+
+  def fall_back_as_file
+    #TODO as file
+  end
+
 end
